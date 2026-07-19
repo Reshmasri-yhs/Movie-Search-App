4a20912ff6f3c9311d6de6bf52d2ea31c820c2ab
Reshmasri-yhs <23ht1a0509@gmail.com>
2026-07-19 23:16:39 +0530

    Add files via upload

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({children}){
+
+  const user=
+  JSON.parse(
+    localStorage.getItem("loggedUser")
+  );
+
+
+  if(!user){
+
+    return <Navigate to="/login"/>;
+
+  }
+
+
+  return children;
+
+}
+
+export default ProtectedRoute;
